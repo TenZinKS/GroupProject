f5ed957bda6bc8f60f9775c172a40af19135ef2e
TenZinKS <tenzinkuenzel2016@gmail.com>
2023-05-30 22:23:43 +0545

    Updated

--- Homepage.py
+++ Homepage.py
@@ -1,20 +1,3 @@
-# from tkinter import *
-
-# root = Tk()
-# root.title("HomePage")
-# # root.iconbitmap()
-# root.geometry("1920x1080")
-
-# def update():
-#     my_label.config(text="New Text")
-
-# my_label = Label(root,text="Old Text",font=("Comfortaa",100))
-# my_label.pack()
-
-# my_label.after(5000,update)
-
-# root.mainloop()
-
 from tkinter import *
 from PIL import ImageTk,Image
 
@@ -22,6 +5,11 @@ root = Tk()
 root.iconbitmap("logo1.ico")
 root.title("Frames")
 root.geometry("1920x1080")
+
+#BMI Function
+def bmi():
+    import bmicalculator.py as bmi
+
 
 #For Background
 Bg = PhotoImage(file="background.png")
@@ -31,30 +19,53 @@ Bg_Label = Label(root,image=Bg)
 Bg_Label.place(x=0,y=0,relwidth=1,relheight=1)
 
 # Frames for the Homepage
-calendarframe= LabelFrame(root,font=("Helvetica",15,"bold"),padx=520)
+calendarframe= LabelFrame(root,font=("Helvetica",15,"bold"),padx=400)
 calendarframe.grid(row=0,column=0)
 
-logoframe = LabelFrame(root,padx=200)
-logoframe.grid(row=0,column=1)
+noteframe = LabelFrame(root,font=("Helvetica",15,"bold"),padx=200)
+noteframe.grid(row=0,column=1)
 
-menuframe= LabelFrame(root,padx=200)
-menuframe.grid(row=1,column=1)
+logoframe = LabelFrame(root,padx=100)
+logoframe.grid(row=0,column=2)
 
-sloganframe = LabelFrame(root,padx=200)
+menuframe= LabelFrame(root,text="Menu",padx=100)
+menuframe.grid(row=1,column=2)
+
+sloganframe = LabelFrame(root,padx=100)
 sloganframe.grid(row=2,column=1)
 
 slogan = Label(text="Slogan")
-slogan.grid(row=3,column=1)
+slogan.grid(row=3,column=2)
 
 
-B1 = Button(logoframe)
+# Buttons for different frames
+
+B1 = Button(logoframe,text ="LOGO")
 B1.pack()
 
-B1 = Button(calendarframe)
+B1 = Button(calendarframe,text="Calendar")
 B1.pack()
 
-B2 = Button(menuframe)
-B2.pack()
+# For the Note Frame
+spotting = Label(noteframe,text="Spotting")
+spotting.grid(row=0,column=0)
+
+
+
+
+# For the MenuFrame
+bmicalc = Button(menuframe,text="BMI Calculator",command = bmi)
+bmicalc.pack()
+
+workout = Button(menuframe,text="WorkOut")
+workout.pack()
+
+doanddont = Button(menuframe,text="Do's & Dont's")
+doanddont.pack()
+
+help = Button(menuframe,text="Help & Support")
+help.pack()
+
 
 #For the Slogan
 
