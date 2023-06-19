f1d2e48b11c0ab2100135231dffae1952a6c6ea5
Abhinav Dhakal <128499381+abhidhakal@users.noreply.github.com>
2023-06-19 19:30:05 +0545

    Update signup.py
    yess

--- signup.py
+++ signup.py
@@ -1,51 +1,64 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
-root = Tk()
+root = Tk() 
 root.geometry("1920x1080")
+root.title("SignUp")
 
-bg = PhotoImage(file="suptheme.png")
-pic_label = Label(root, image=bg)
-pic_label.place(x=0, y=0, relwidth=1, relheight=1)
+title = Label(root, text="SIGN UP",fg="black", font=("Arial", 48, "bold"))
+title.pack(side="top")
 
-frame=Frame(root, width=500, height=500, bg="white")
-frame.place(x=440, y=80 )
+# Function after clicking signup
+def signedup():
+    root.destroy()
+    import Homepage as home
 
-Label(frame, text="SIGN UP", bg="white", fg="black", font=("Comic Sans MS", 36, "bold"), pady=40).grid(row=0, column=2)
+# Function after clicking back to home
+def logingo():
+    root.destroy()
+    import main as login
 
-a = Label(frame, bg="white", fg="black").grid(row=0, column=0)
+# Main Frame
+frame=Frame(root, width=1920, height=1080, bg="white",padx=150, pady=150)
+frame.pack(side="bottom")
 
-fullname=Label(frame, text="                 Full Name: ", font=("Arial", 15), bg="white", fg="black", pady=5).grid(row=1, column=1)
-fullname_entry = Entry(frame, width=25, bg="white", fg="black").grid(row=1, column=2)
+# PERSONAL DETAILS
 
-contact=Label(frame, text="                 Contact: ", font=("Arial", 15), bg="white", fg="black", pady=5).grid(row=2, column=1)
-contact_entry = Entry(frame, width=25, bg="white", fg="black").grid(row=2, column=2)
+fullname=Label(frame, text="                 Full Name: ", font=("Arial", 15), bg="white", fg="black", pady=5).grid(row=1, column=1, sticky="w")
+fullname_entry = Entry(frame, width=25, bg="white", fg="black").grid(row=1, column=2, sticky="w")
 
-age = Label(frame, text="                 Age: ", font=("Arial", 15), bg="white", fg="black", pady=5).grid(row=3, column=1)
-age_entry = Entry(frame, width=25, bg="white", fg="black").grid(row=3, column=2)
+contact=Label(frame, text="                 Contact: ", font=("Arial", 15), bg="white", fg="black", pady=5).grid(row=2, column=1, sticky="w")
+contact_entry = Entry(frame, width=25, bg="white", fg="black").grid(row=2, column=2, sticky="w")
 
-email = Label(frame, text="                 E-mail: ", font=("Arial", 15), bg="white", fg="black", pady=5).grid(row=4, column=1)
-email_entry = Entry(frame, width=25, bg="white", fg="black").grid(row=4, column=2)
+age = Label(frame, text="                 Age: ", font=("Arial", 15), bg="white", fg="black", pady=5).grid(row=3, column=1, sticky="w")
+age_entry = Entry(frame, width=25, bg="white", fg="black").grid(row=3, column=2, sticky="w")
 
-new_pw = Label(frame, text="                 New password: ", font=("Arial", 15), bg="white", fg="black").grid(row=5, column=1)
-new_pw_entry = Entry(frame, width=25, show="*", bg="white", fg="black").grid(row=5, column=2, pady=20)
+email = Label(frame, text="                 E-mail: ", font=("Arial", 15), bg="white", fg="black", pady=5).grid(row=4, column=1, sticky="w")
+email_entry = Entry(frame, width=25, bg="white", fg="black").grid(row=4, column=2, sticky="w")
 
-confirm_pw = Label(frame, text="                 Confirm password: ", font=("Arial", 15), bg="white", fg="black").grid(row=6, column=1)
-confirm_pw_entry = Entry(frame, width=25, show="*", bg="white", fg="black").grid(row=6, column=2)
+# NEW PASSWORD
+new_pw = Label(frame, text="                 New password: ", font=("Arial", 15), bg="white", fg="black").grid(row=5, column=1, sticky="w")
+new_pw_entry = Entry(frame, width=25, show="*", bg="white", fg="black").grid(row=5, column=2, pady=20, sticky="w")
 
-#Questions
-last_period = Label(frame, text="                 Your Last Period: ", font=("Arial", 15), bg="white", fg="black", pady=50).grid(row=7, column=1)
-last_period_entry = Entry(frame, width=25, bg="white", fg="black").grid(row=7, column=2)
+confirm_pw = Label(frame, text="                 Confirm password: ", font=("Arial", 15), bg="white", fg="black").grid(row=6, column=1, sticky="w")
+confirm_pw_entry = Entry(frame, width=25, show="*", bg="white", fg="black").grid(row=6, column=2, sticky="w")
 
-pills = Label(frame, text="                 Do you take pills?", font=("Arial", 15), bg="white", fg="black").grid(row=8, column=1)
-pills = Checkbutton(frame, bg="white", fg="black").grid(row=8, column=2)
+# QUESTIONS
+last_period = Label(frame, text="                 Your Last Period: ", font=("Arial", 15), bg="white", fg="black", pady=50).grid(row=7, column=1, sticky="w")
+last_period_entry = Entry(frame, width=25, bg="white", fg="black").grid(row=7, column=2, sticky="w")
 
-last_pill = Label(frame, text="                 Your last pill intake: ", font=("Arial", 15), bg="white", fg="black", pady=50).grid(row=9, column=1)
-last_pill_entry = Entry(frame, width=25, bg="white", fg="black").grid(row=9, column=2)
+pills = Label(frame, text="                 Do you take pills?", font=("Arial", 15), bg="white", fg="black").grid(row=8, column=1, sticky="w")
+pills = Checkbutton(frame, bg="white", fg="black").grid(row=8, column=2, sticky="w")
 
-signup=Button(frame,text="Register Now",fg="#a94da0",relief=RAISED, bg="white", pady=10)
-signup.grid(row=10,column=2)
+last_pill = Label(frame, text="                 Your last pill intake: ", font=("Arial", 15), bg="white", fg="black", pady=50).grid(row=9, column=1, sticky="w")
+last_pill_entry = Entry(frame, width=25, bg="white", fg="black").grid(row=9, column=2, sticky="w")
 
-# lol=Label(frame, text="                      ", pady=30, bg="white").grid(row=10, column=1)
+# SIGNUP BUTTON
+signup=Button(frame,text="Register Now",fg="#a94da0",relief=RAISED, bg="white", pady=10, width=11, command=signedup)
+signup.grid(row=10,column=2, sticky="w")
 
+# RETURN TO LOGIN BUTTON
+home = Button(frame, text="Return To Login", fg="#a94da0",relief=RAISED, bg="white", pady=10, width=11, command=logingo)
+home.grid(row=11, column=2, sticky="w", pady=20)
 
 root.mainloop()
