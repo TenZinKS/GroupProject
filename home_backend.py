ce5b2a15ff9fbc86a90666ac593599289da89b8c
TenZinKS <96676013+TenZinKS@users.noreply.github.com>
2023-08-10 16:14:25 +0545

    Update home_backend.py

--- home_backend.py
+++ home_backend.py
@@ -7,57 +7,87 @@ def connect():
     A function to create connection with database and create a cursor.
     It also checks whether there is a table present if not it creates a new one.
     '''
-    conn= sqlite3.connect('notes.db')
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS note(date text, spotting text, fluid text, sex text, temperature integer, pain text, pill text, mood text)")
-    conn.commit()
-    conn.close()
+    try:
+        conn = sqlite3.connect('notes.db')
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS note(date text, spotting text, fluid text, sex text, temperature integer, pain text, pill text, mood text)")
+        conn.commit()
+    except sqlite3.Error as e:
+        print("An error occurred:", e)
+    finally:
+        if conn:
+            conn.close()
 
 def insert(date,spotting,fluid,sex,temperature,pain,pill,mood):
     """
     This function helps to insert the data into the database.
     """
-    conn= sqlite3.connect('notes.db')
-    cur = conn.cursor()
-    cur.execute("INSERT INTO note VALUES (?,?,?,?,?,?,?,?)",(date,spotting,fluid,sex,temperature,pain,pill,mood))
-
-    conn.commit()
-    conn.close()
+    try:
+        conn = sqlite3.connect('notes.db')
+        cur = conn.cursor()
+        cur.execute("INSERT INTO note VALUES (?,?,?,?,?,?,?,?)",(date,spotting,fluid,sex,temperature,pain,pill,mood))
+        conn.commit()
+    except sqlite3.Error as e:
+        print("An error occurred:", e)
+    finally:
+        if conn:
+            conn.close()
 
 def view():
     """
     This function allows to retrieve all the data from the table of the database.
     Returns rows as a tuple 
     """
-    conn= sqlite3.connect('notes.db')
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM note")
-    rows = cur.fetchall()
-    conn.close()
-    return rows
+    try:
+        conn = sqlite3.connect('notes.db')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM note")
+        rows = cur.fetchall()
+        return rows
+    except sqlite3.Error as e:
+        print("An error occurred:", e)
+    finally:
+        if conn:
+            conn.close()
 
 # TO WORK
 def search(date):
-    conn= sqlite3.connect('notes.db')
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM note WHERE date=?", (date,))
-    rows = cur.fetchall()
-    conn.close()
-    return rows
+    try:
+        conn = sqlite3.connect('notes.db')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM note WHERE date=?", (date,))
+        rows = cur.fetchall()
+        return rows
+    except sqlite3.Error as e:
+        print("An error occurred:", e)
+        return None  # Return None to indicate failure
+    finally:
+        if conn:
+            conn.close()
 
 def delete(date):
-    conn= sqlite3.connect('notes.db')
-    cur = conn.cursor()
-    cur.execute("DELETE FROM note WHERE date=?",(date,))
-    conn.commit()
-    conn.close()
+    try:
+        conn = sqlite3.connect('notes.db')
+        cur = conn.cursor()
+        cur.execute("DELETE FROM note WHERE date=?", (date,))
+        conn.commit()
+    except sqlite3.Error as e:
+        print("An error occurred:", e)
+    finally:
+        if conn:
+            conn.close()
 
 def update(date,spotting,fluid,sex,temperature,pain,pill,mood):
-    conn= sqlite3.connect('notes.db')
-    cur = conn.cursor()
-    cur.execute("UPDATE note SET spotting=?,fluid=?,sex=?,temperature=?,pain=?,pill=?,mood=? WHERE date=?",(spotting,fluid,sex,temperature,pain,pill,mood,date))
-    conn.commit()
-    conn.close()
+    try:
+        conn = sqlite3.connect('notes.db')
+        cur = conn.cursor()
+        cur.execute("UPDATE note SET spotting=?,fluid=?,sex=?,temperature=?,pain=?,pill=?,mood=? WHERE date=?", (spotting,fluid,sex,temperature,pain,pill,mood,date))
+        conn.commit()
+    except sqlite3.Error as e:
+        print("An error occurred:", e)
+    finally:
+        if conn:
+            conn.close()
 
 #To call the function anytime it gets imported.
 connect()
